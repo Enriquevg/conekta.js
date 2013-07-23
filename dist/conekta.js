ebdccac1e705a6cddc39989afe7a765f3164fe51
Jack London <leo2fischer@gmail.com>
2013-07-23 17:04:27 -0400

    license_dates

--- dist/conekta.js
+++ dist/conekta.js
@@ -1,4 +1,4 @@
-/*!conekta.js v0.1.0 | 2013 Conekta | https://github.com/conekta/conekta.js/blob/master/LICENSE-MIT.txt
+/*!conekta.js v0.1.0 | 2013- Conekta | https://github.com/conekta/conekta.js/blob/master/LICENSE-MIT.txt
 */
 
 
